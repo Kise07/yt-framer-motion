fcf5df7af69827443cb7330355428652f8231eec
kise <kaiazmi1412@gmail.com>
2026-07-09 01:39:14 +0530

    feat: animate-sequences-in-motion-lengthy
    Sequence-loader-Animation:
    - Button-sequence-loader[lengthy].
    - SVGs loader + check-icon.

---
+++ animation-sequences-in-motion-6/app/components/animation-sequences.tsx
@@ -0,0 +1,162 @@
+"use client";
+import { useAnimate, motion } from "motion/react";
+
+export const AnimationSequences = () => {
+  const [scope, animate] = useAnimate();
+  const startAnimating = async () => {
+    // first phase of loader
+    await animate(
+      ".loader",
+      {
+        opacity: 1,
+        width: "2rem",
+      },
+      {
+        duration: 0.1,
+      },
+    );
+    await animate(
+      ".loader",
+      {
+        rotate: 360 * 4,
+      },
+      {
+        duration: 2,
+      },
+    );
+    // second phase of loader
+    await animate(
+      ".loader",
+      {
+        opacity: 0,
+        scale: 0,
+      },
+      {
+        duration: 0.1,
+      },
+    );
+    // text-disappearing
+    await animate(
+      ".text",
+      {
+        display: "none",
+      },
+      {
+        duration: 0.1,
+      },
+    );
+    // first phase of button
+    await animate(
+      "button",
+      {
+        width: "5rem",
+        borderRadius: "1000px",
+      },
+      {
+        duration: 0.3,
+      },
+    );
+    // second phase of button
+    await animate(
+      "button",
+      {
+        opacity: 1,
+        scale: [1, 1.2, 0.8, 1],
+        backgroundImage: "linear-gradient(to right, #00ff99, #00ccff)",
+      },
+      {
+        duration: 0.8,
+      },
+    );
+    await animate(
+      "button",
+      {
+        backgroundImage: "linear-gradient(to right, #00ff99, #00ccff)",
+      },
+      {
+        duration: 0.8,
+      },
+    );
+    // second phase of check-icon
+    await animate(
+      ".check-icon",
+      {
+        opacity: 1,
+      },
+      {
+        duration: 0.1,
+      },
+    );
+    await animate(
+      ".check-icon path",
+      {
+        pathLength: 1,
+      },
+      {
+        duration: 0.3,
+      },
+    );
+  };
+
+  return (
+    <div
+      ref={scope}
+      className="relative w-[30rem] h-20 flex items-center justify-center"
+    >
+      <motion.button
+        onClick={startAnimating}
+        style={{
+          width: "30rem",
+        }}
+        className="h-20 rounded-lg flex items-center justify-center bg-gradient-to-r from-purple-500 via-voilet-600 to-indigo-500 text-white font-medium cursor-pointer"
+      >
+        {/* loader svg */}
+        <motion.svg
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="loader h-5 w-5 text-white"
+          initial={{
+            width: "0rem",
+          }}
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M12 3a9 9 0 1 0 9 9" />
+        </motion.svg>
+        <span className="text">Purchase Now ($169)</span>
+      </motion.button>
+
+      {/* check-icon svg */}
+      <motion.svg
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="#FFFFFF"
+        strokeWidth={3}
+        className="check-icon h-8 w-8 absolute inset-0 m-auto z-50 pointer-events-none"
+        style={{
+          opacity: 0,
+        }}
+      >
+        {/* first phase of check-icon */}
+        <motion.path
+          initial={{
+            pathLength: 0,
+          }}
+          transition={{
+            duration: 0.3,
+            type: "tween",
+            ease: "easeInOut",
+          }}
+          strokeLinecap="round"
+          strokeDashoffset="round"
+          d="M5 13l4 4L19 7"
+        />
+      </motion.svg>
+    </div>
+  );
+};
--- animation-sequences-in-motion-6/app/page.tsx
+++ animation-sequences-in-motion-6/app/page.tsx
@@ -1,9 +1,10 @@
 import { AnimatedText } from "./components/animated-text";
+import { AnimationSequences } from "./components/animation-sequences";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-black flex items-center justify-center">
-      <AnimatedText />
+      <AnimationSequences />
     </div>
   );
 }
