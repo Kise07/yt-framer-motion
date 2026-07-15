41f05f7ceb2164bd7ff3d321455fb35dea34556f
kise <kaiazmi1412@gmail.com>
2026-07-16 00:04:08 +0530

    feat: all-blog-done

--- minimal-portfolio-with-motion-7/app/blog/page.tsx
+++ minimal-portfolio-with-motion-7/app/blog/page.tsx
@@ -1,5 +1,7 @@
 import { Container } from "@/app/components/container";
 import { Metadata } from "next";
+import { getBlogs } from "../utils/mdx";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "All blogs - Sebas",
@@ -7,7 +9,12 @@ export const metadata: Metadata = {
     "All my general wisdom and thoughts about software engineering, design, and life.",
 };
 
-export default function blogspage() {
+export default async function blogspage() {
+  const allBlogs = await getBlogs();
+
+  const truncate = (str: string, length: number) => {
+    return str.length > length ? str.substring(0, length) + "..." : str;
+  };
   return (
     <div className="flex min-h-screen items-start justify-start">
       <Container className="min-h-[200vh] px-10 md:pt-20 md:pb-10">
@@ -19,6 +26,28 @@ export default function blogspage() {
           beautiful and functional user experiences. I specialize in front-end
           development, UI/UX design, and web development.
         </p>
+        <div className="flex flex-col gap-4 py-10">
+          {allBlogs.map((blog, idx) => (
+            <Link key={idx} href={`/blog/${blog.slug}`}>
+              <div className="flex items-center justify-between">
+                <h2 className="text-primary text-base font-bold tracking-tight">
+                  {blog.title}
+                </h2>
+                <p className="text-secondary text-sm md:text-sm">
+                  {new Date(blog.date).toLocaleDateString("en-us", {
+                    weekday: "short",
+                    year: "numeric",
+                    month: "short",
+                    day: "numeric",
+                  })}
+                </p>
+              </div>
+              <p className="text-secondary max-w-lg pt-2 text-sm md:text-sm">
+                {truncate(blog.description || "", 150)}
+              </p>
+            </Link>
+          ))}
+        </div>
       </Container>
     </div>
   );
--- minimal-portfolio-with-motion-7/app/utils/mdx.ts
+++ minimal-portfolio-with-motion-7/app/utils/mdx.ts
@@ -2,6 +2,13 @@ import { promises as fs } from "fs";
 import { compileMDX } from "next-mdx-remote/rsc";
 import path from "path";
 
+type FrontMatter = {
+  title: string;
+  description: string;
+  date: string;
+  image: string;
+};
+
 export const getSingleBlog = async (slug: string) => {
   try {
     const singleBlog = await fs.readFile(
@@ -13,12 +20,7 @@ export const getSingleBlog = async (slug: string) => {
       return null;
     }
 
-    const { content, frontmatter } = await compileMDX<{
-      title: string;
-      description: string;
-      image: string;
-      date: string;
-    }>({
+    const { content, frontmatter } = await compileMDX<FrontMatter>({
       source: singleBlog,
       options: {
         parseFrontmatter: true,
@@ -30,4 +32,40 @@ export const getSingleBlog = async (slug: string) => {
     console.error(`Error reading blog file for slug "${slug}"`, error);
     return null;
   }
+};
+
+export const getBlogs = async () => {
+  const files = await fs.readdir(path.join(process.cwd(), "app/data"));
+
+  const allBlogs = (
+    await Promise.all(
+      files.map(async (file) => {
+        const slug = file.replace(/\.mdx$/, "");
+        const frontmatter = await getBlogFrontmatterBySlug(slug);
+        return frontmatter ? { slug, ...frontmatter } : null;
+      }),
+    )
+  ).filter(Boolean) as (FrontMatter & { slug: string })[];
+
+  return allBlogs;
+};
+
+const getBlogFrontmatterBySlug = async (slug: string) => {
+  const singleBlog = await fs.readFile(
+    path.join(process.cwd(), "app/data", `${slug}.mdx`),
+    "utf-8",
+  );
+
+  if (!singleBlog) {
+    return null;
+  }
+
+  const { frontmatter } = await compileMDX<FrontMatter>({
+    source: singleBlog,
+    options: {
+      parseFrontmatter: true,
+    },
+  });
+
+  return frontmatter;
 };
